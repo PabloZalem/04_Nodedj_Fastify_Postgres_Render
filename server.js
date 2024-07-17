96799ec509f8668ac297442a00a4cee0e570d207
PabloZalem <zalem.pablo@yahoo.com>
2024-07-16 21:19:40 -0300

    API NODE COM POSTGRES

---
+++ server.js
@@ -0,0 +1,54 @@
+import { fastify } from "fastify";
+// import { databaseMemomy } from "./databaseMemory.js";
+import { DatabasePostegres } from "./database-postgres.js";
+
+const server = fastify();   
+// const database = new databaseMemomy();
+const database = new DatabasePostegres();
+
+
+server.post('/video', async (req, res) => {
+    
+    const { title, description, duration } = req.body
+    
+    await database.create({
+        title, 
+        description,
+        duration,
+    })
+
+    return res.status(201).send();
+})
+
+server.get('/videos', async (req, res) => {
+    const search = req.query.search
+
+    const videos = await database.list(search);
+    
+    return videos
+})
+
+server.put('/video/:id', async (req, res) => {
+    const videoId = req.params.id
+    const { title, description, duration } = req.body
+
+    await database.update(videoId, {
+            title, 
+            description,
+            duration,
+    })
+
+    return res.status(204).send()
+})
+
+server.delete('/video/:id', async (req, res) => {
+    const videoId = req.params.id
+
+    await database.delete(videoId)
+
+    return res.status(204).send()
+})
+
+server.listen({
+    port: process.env.PORT ?? 8000,
+});
